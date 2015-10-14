bf420621d29798fa99d386cbf0a6bd807ebce6a2
Michelle Lee <mlee5394@uw.edu>
2015-10-13 17:14:16 -0700

    completed lab

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,3 +1,43 @@
 /**
  * application script for index.html
  */
+
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict'; // they made it into a string b/c IE8 would silently ignored it.
+
+    function forEachElement(collection, fn) {
+        var num;
+        for(num = 0; num < collection.length; num++) {
+            fn(collection[num]);
+        }
+    }
+
+
+    var clickMeButton = document.getElementById("click-me");
+    clickMeButton.addEventListener('click', function() {
+        var alerts = document.querySelectorAll('.alert');
+        forEachElement(alerts,function(alert) {
+            alert.style.display = 'block';
+        });
+    });
+
+    var closeButtons = document.querySelectorAll('.alert .close');
+    //var idx;
+    //var closeButton;
+    // declare variables outside of for blocks
+    //for(idx = 0; idx < closeButtons.length; idx++) {
+    //    closeButton = closeButtons[idx];
+    //    closeButton.addEventListener('click', function() {
+    //        this.parentElement.style.display = 'none';
+    //    });
+    //}
+    // above is the same as:
+    forEachElement(closeButtons, function(button) {
+        button.addEventListener('click', function() {
+            button.parentElement.style.display = 'none';
+        });
+    });
+
+
+
+});
