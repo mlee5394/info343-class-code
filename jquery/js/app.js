b0d5a9f0f45ea27cf2f8888a011e52693896d759
Michelle Lee <mlee5394@uw.edu>
2015-10-15 17:11:39 -0700

    tutorial done

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -5,4 +5,24 @@
     open weather API: 
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
+// when the DOM content has been loaded...
+$(function() {
+    'use strict';
+    $('a').attr('target', '_blank');
+    $('article').hide().fadeIn(100).fadeOut(100).fadeIn(500);
 
+    $('#toggle-article').click(function() {
+       //$('article').slideToggle(1500);
+        $('article').text('You clicked');
+    });
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f'
+    $.getJSON(url)
+        .then(function(data) {
+            // console.log(data);
+            // this following code puts the current temperature and updates it with the data given
+            var temperature = data.main.temp;
+            $('#temp').text(Math.round(temperature));
+    });
+
+});
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,28 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+$(function() {
+
+    function createMap(loc, zoom) {
+        'use strict';
+
+        // set view is a latittude number and longitude number and zoom level
+        var map = L.map('map').setView(loc, zoom);
+
+        // every layer should have a .addTo(map);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.circleMarker(loc, zoom).addTo(map).bindPopup('Hello');
+
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            createMap([pos.coords.latitude, pos.coords.longitude], 15);
+        });
+    } else {
+        createMap([47.6097, -122.3331], 12);
+    }
+});
