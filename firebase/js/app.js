3eb22aba8f379d779f502b73f56c5923951a6ea1
Michelle Lee <mlee5394@uw.edu>
2015-11-05 16:59:21 -0800

    yay homework...

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,24 @@
 
 angular.module('ChatApp', ['firebase'])
     .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
+    .controller('ChatController', function($scope, $firebaseArray, firebaseUrl) {
         //TODO: implement our chat controller
+        // create reference to the Firebase
+        var ref = new Firebase(firebaseUrl);
+        ref.limitToLast(1000);
+        $scope.messages = $firebaseArray(ref);
+
+        // initialize form fields
+        $scope.name = null;
+        $scope.body = null;
+
+        $scope.sendMessage = function() {
+            $scope.messages.$add({
+                name: $scope.name,
+                body: $scope.body,
+                createdAt: Firebase.ServerValue.TIMESTAMP
+            });
+
+            $scope.body = null;
+        } // sendMessage()
     });
